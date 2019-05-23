e0a3239e4484ee80b1f167dbaa151b89aff9f8fb
hbyrd14 <herbert.byrd@icloud.com>
2019-05-22 23:42:25 -0400

    Initial Uploads

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#video").hide();
+    $("#video").fadeIn(4000);
+    $("button").click(function(){
+        window.open('testScript.pdf', '_blank');
+    });
+    
+});
