e6caa7db4527e541a84cae4556f317a7f9982681
hbyrd14 <herbert.byrd@icloud.com>
2021-07-18 15:10:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
     $("#video").hide();
     $("#video").fadeIn(4000);
+    $("button").hide();
     $("button").click(function(){
         window.open('testScript.pdf', '_blank');
     });
+    document.getElementById("video").addEventListener('ended',myHandler,false);
+    function myHandler(e) {
+        $("button").fadeIn(4000);
+    }
     
 });
